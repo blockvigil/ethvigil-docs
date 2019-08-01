add02f8ec557047b04c7e5fa393ab8e8455f5f31
Swaroop Hegde <email@swaroophegde.com>
2019-08-02 01:15:03 +0530

    renamed to ethvigil-docs

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -32,7 +32,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'ethvigil-cli',
+  projectName: 'ethvigil-docs',
   organizationName: 'blockvigil',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
@@ -77,7 +77,7 @@ scrollToTop: true,
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
   copyright: `Copyright © ${new Date().getFullYear()} BlockVigil Inc.`,
-  repoUrl: 'https://github.com/BlockVigil/ethvigil-cli',
+  repoUrl: 'https://github.com/BlockVigil/ethvigil-docs',
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
     theme: 'default',
